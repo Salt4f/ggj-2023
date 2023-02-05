644e2d656d761e78fcddf9ad642eca4abc3f2f47
Salt4f <47352634+Salt4f@users.noreply.github.com>
2023-02-05 15:01:55 +0100

    feat: Click SFX working

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,24 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject credits;
+    public AudioSource clickSource;
+
+    private float timer;
+    private readonly float clickDuration = 0.5f;
+
+    private int sceneToLoad;
+
+    private void Awake()
+    {
+        sceneToLoad = -1;
+    }
+
+    private void Update()
+    {
+        if (timer > 0 && sceneToLoad != -1)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+    }
 
     public void OnStart()
     {
-        SceneManager.LoadScene(1);
+        timer = clickDuration;
+        sceneToLoad = 1;
+        clickSource.Play();
     }
 
     public void OnCreditsOpen()
     {
         credits.SetActive(true);
+        clickSource.Play();
     }
 
     public void OnCreditsClose()
     {
         credits.SetActive(false);
+        clickSource.Play();
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -12,12 +12,30 @@ public class UIManager : MonoBehaviour
 
     public GameObject deathPanel;
     public TMP_Text stepsDeathCounter;
+    public AudioSource clickSource;
 
+    private float timer;
+    private readonly float clickDuration = 0.5f;
+
+    private int sceneToLoad;
 
     private void Awake()
     {
         GameManager.Instance.ui = this;
+        sceneToLoad = -1;
         SetSteps(0);
+    }
+
+    private void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
     }
 
     public void SetSteps(uint steps)
@@ -34,12 +52,18 @@ public class UIManager : MonoBehaviour
 
     public void OnMainMenu()
     {
-        SceneManager.LoadScene(0);
+        if (sceneToLoad != -1) return;
+        sceneToLoad = 0;
+        timer = clickDuration;
+        clickSource.Play();
     }
 
     public void OnRestart()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        if (sceneToLoad != -1) return;
+        sceneToLoad = SceneManager.GetActiveScene().buildIndex;
+        timer = clickDuration;
+        clickSource.Play();
     }
 
 }
